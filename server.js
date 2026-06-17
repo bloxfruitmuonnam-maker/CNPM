e8c482624be2039e2851e8c1be8f91fa8279d4e6
sontrinh3927-ntm <sontrinh3927@gmail.com>
2026-06-17 08:18:37 +0700

    Update server.js

--- server.js
+++ server.js
@@ -110,3 +110,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server FreshKeep đang hoạt động tại cổng ${PORT}`);
 });
+
+
+const shareRoutes = require("./routes/share");
+app.use("/share", shareRoutes);
